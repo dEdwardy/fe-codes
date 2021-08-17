034cba23f947a446c2a983c64a66634707252fc5
edw4rd6731 <957196958@qq.com>
2021-08-18 00:26:38 +0800

    f

--- index.js
+++ index.js
@@ -65,16 +65,37 @@ function _new(fn, ...args) {
 // 4.手写promise   见promsie.js
 
 // 5.debounce && throttle
-function debounce(fn,wait) {
-  let timer = null
-  return function(...args) {
-    let ctx = this;
-    if(timer){
-      clearTimeout(timer)
-      timer = null
+
+function debounce(fn, wait = 500) {
+  let timer = null;
+  return function () {
+    let context = this,
+      args = arguments;
+
+    // 如果此时存在定时器的话，则取消之前的定时器重新记时
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
     }
+
+    // 设置定时器，使事件间隔指定事件后执行
     timer = setTimeout(() => {
-      fn.apply(ctx,args)
+      fn.apply(context, args);
     }, wait);
-  }
+  };
+}
+function throttle(fn, delay = 1000) {
+  let curTime = Date.now();
+
+  return function () {
+    let context = this,
+      args = arguments,
+      nowTime = Date.now();
+
+    // 如果两次时间间隔超过了指定时间，则执行函数。
+    if (nowTime - curTime >= delay) {
+      curTime = Date.now();
+      return fn.apply(context, args);
+    }
+  };
 }
